c7d43a21c9a029d39bda5a8416f1e470820b8b84
natsuyasai <yasai038tomato@gmail.com>
2023-09-10 17:23:18 +0900

    redis対応

--- demo/src/main/java/com/example/demo/dto/DemoData.java
+++ demo/src/main/java/com/example/demo/dto/DemoData.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Table(name="demodata", schema = "data")
 @Entity
 public class DemoData {
